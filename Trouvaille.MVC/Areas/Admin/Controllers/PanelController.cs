21d6cf79c5c05a8597a5b792935348c0c2a53fb0
TeodSt <teod_stoyanova@abv.bg>
2017-03-17 17:21:46 +0200

    Added admin area and authorization

---
+++ Trouvaille.MVC/Areas/Admin/Controllers/PanelController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trouvaille.MVC.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class PanelController : Controller
+    {
+        // GET: Admin/Panel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
