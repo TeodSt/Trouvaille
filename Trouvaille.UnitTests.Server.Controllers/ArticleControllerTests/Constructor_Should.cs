d148752e9640b7d54f5e116456d86bc0370fca2e
TeodSt <teod_stoyanova@abv.bg>
2017-03-25 12:24:35 +0200

    Added single Controller test

---
+++ Trouvaille.UnitTests.Server.Controllers/ArticleControllerTests/Constructor_Should.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trouvaille.MVC.Controllers;
+using Trouvaille.Services.Contracts;
+
+namespace Trouvaille.UnitTests.Server.Controllers.ArticleControllerTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        //ArticleController(IMappingService mappingService, IArticleService articleService)
+        [Test]
+        public void ThrowArgumentNullException_WhenMappingServiceIsNull()
+        {
+            // Arrange
+            var mockedArticleService = new Mock<IArticleService>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new ArticleController(null, mockedArticleService.Object));
+        }
+    }
+}
